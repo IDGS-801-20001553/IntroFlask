a8117b2f33d7c112be129d8c7fa28361b4ddb0e1
IDGS-801-20001553 <junonyu1234@gmail.com>
2025-02-12 23:42:49 -0600

    cinepolis_Flask 12/02/2025

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request , jsonify
 
-app=Flask(__name__)
+app = Flask(__name__, template_folder="template")
 
-@app.route("/")
-def index():
-    return "<h1>hello word!</h1>"
+
 
 @app.route("/")
 def index():
@@ -18,7 +16,7 @@ def ejemplo1():
 
 
 @app.route("/ejemplo2")
-def ejemplo1():
+def ejemplo2():
     return render_template("ejemplo2.html")
 
 
@@ -61,8 +59,65 @@ def operas():
 
         </form>
             '''
+@app.route("/operasBass")
+def operas1():
+    return render_template("operaBass.html")
 
 
+@app.route("/resultado",methods=["POST","GET"])
+def result():
+    n1=request.form.get("n1")
+    n2=request.form.get("n2")
+    return "la multiplicación de {}x {} es {}".format(n1,n2,str(int(n1)*int(n2)))
+
+@app.route("/resultado2", methods=["POST"])
+def resultado2():
+    
+        n1 = float(request.form.get("n1"))
+        n2 = float(request.form.get("n2"))
+        operacion = request.form.get("operacion")
+
+        if operacion == "suma":
+            resultado = n1 + n2
+        elif operacion == "resta":
+            resultado = n1 - n2
+        elif operacion == "multiplicacion":
+            resultado = n1 * n2
+        elif operacion == "division":
+            resultado = n1 / n2 
+        else:
+            resultado = "Operación no válida"
+
+        return str(resultado)
+
+@app.route("/cinepolis")
+def cine():
+    return render_template("cinepolis.html")
+
+@app.route("/calculo_Cine", methods=["POST"])
+def calculo_Cine():
+    data = request.json
+    personas = int(data.get("cantidadPersonas", 0))
+    boletos = int(data.get("cantidadBoletos", 0))
+    cineco = data.get("tarjeta")
+    
+    max_boletos = personas * 7
+    precio = 12
+    
+    if boletos > max_boletos:
+        return jsonify({"error": f"Solo puedes comprar hasta {max_boletos} boletos."}), 400
+    
+    total = boletos * precio
+    
+    if boletos > 5:
+        total *= 0.85
+    elif boletos >= 3:
+        total *= 0.90
+    
+    if cineco == "si":
+        total *= 0.90
+    
+    return jsonify({"total": f"${total:.2f}"})
 
 if __name__ == "__main__":
     app.run(debug=True,port=3000)
